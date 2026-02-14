4ed040aaa4004c774c6a7c4b7605a6b7f7427a38
TrupleCPU <janamaya438@gmail.com>
2026-02-14 22:35:07 +0800

    update: strings

--- app/src/main/java/com/example/compose_article/MainActivity.kt
+++ app/src/main/java/com/example/compose_article/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,34 +52,30 @@ fun Display(modifier: Modifier = Modifier){
     ) {
         DisplayImage()
 
-        DisplayText(
-            firstText = "Jetpack Compose tutorial",
-            secondText = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-            thirdText = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
-            )
+        DisplayText()
     }
 }
 
 @Composable
-fun DisplayText(firstText: String, secondText: String, thirdText: String, modifier: Modifier = Modifier){
+fun DisplayText(modifier: Modifier = Modifier){
     Column(
         verticalArrangement = Arrangement.Top,
         modifier = modifier
     ) {
         Text (
-            text = firstText,
+            text = stringResource(R.string.first_text),
             fontSize = 24.sp,
             modifier = Modifier
                 .padding(16.dp)
         )
         Text (
-            text = secondText,
+            text = stringResource(R.string.second_text),
             textAlign = TextAlign.Justify,
             modifier = Modifier
                 .padding(16.dp)
         )
         Text (
-            text = thirdText,
+            text = stringResource(R.string.third_text),
             textAlign = TextAlign.Justify,
             modifier = Modifier
                 .padding(16.dp)
